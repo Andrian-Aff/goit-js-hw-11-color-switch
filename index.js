1691f5bddd4271a08bf7efdd7304fafc5213bad6
Andrian-Aff <affear.123@gmail.com>
2021-08-11 23:34:24 +0300

    finish color-swatch

--- index.js
+++ index.js
@@ -10,24 +10,44 @@ const colors = [
 const refs = {
     startBtn: document.querySelector('[data-action="start"]'),
     stopBtn: document.querySelector('[data-action="stop"]'),
+    body: document.querySelector('body')
 };
 
 refs.startBtn.addEventListener('click', onStartBtnClick);
-refs.stopBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
 
-const isActive = false;
+let isActive = false;
+let intervalId = null;
+let colorId = null;
 
 function onStartBtnClick() {
-  setInterval(() => {
-      if(isActive) {
-          refs.startBtn.classList.add('disabled')
-      }
-
+    isActive = true;
+    if(isActive) {
+        refs.startBtn.setAttribute('disabled', true);
+    };
+    
+    intervalId = setInterval(() => {
+    refs.body.style.backgroundColor = colors[randomIntegerFromInterval(colors)];
+    return intervalId; 
 
   }, 1000);
 };
 
+function onStopBtnClick() {
+    isActive = false;
+    clearInterval(intervalId);
+    refs.startBtn.removeAttribute('disabled');
+    refs.body.removeAttribute('style');
+ 
+    
+    
+};
 
-  const randomIntegerFromInterval = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  };
+
+const randomIntegerFromInterval = (colors) => {    
+    let newColorId = Math.floor(Math.random() * (colors.length));
+    if (colorId === newColorId){
+        return randomIntegerFromInterval(colors);
+    }    
+    return colorId = newColorId;
+   };
